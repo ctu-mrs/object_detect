65174191b88546d3ae54af4115c1aafd68d81557
Matouš Vrba <vrbamato@MyslPlacka>
2019-06-27 18:53:58 +0200

    fixed (hopefully) occasional crashing at startup due to some OCl magic, upgraded lut generation

--- src/SegConf.cpp
+++ src/SegConf.cpp
@@ -11,11 +11,13 @@ namespace object_detect
   {
     public:
         parallelHSVLUT(
+            cv::Mat rgb2hsv_lut,
             const double hue_lower, const double hue_higher, const bool overflow,
             const double sat_lower, const double sat_higher,
             const double val_lower, const double val_higher,
             lut_elem_t color_label, lut_t& lut)
-          : hue_lower(hue_lower), hue_higher(hue_higher), overflow(overflow),
+          : rgb2hsv_lut(rgb2hsv_lut),
+            hue_lower(hue_lower), hue_higher(hue_higher), overflow(overflow),
             sat_lower(sat_lower), sat_higher(sat_higher),
             val_lower(val_lower), val_higher(val_higher),
             color_label(color_label), lut(lut)
@@ -24,20 +26,16 @@ namespace object_detect
 
         void operator()(const cv::Range &range) const
         {
-          cv::Mat color_rgb;
-          cv::Mat color_hsv;
           for (auto r = range.start; r < range.end; r++)
           {
             for (size_t g = 0; g < lut_dim; g++)
             {
               for (size_t b = 0; b < lut_dim; b++)
               {
-                color_rgb = cv::Mat(cv::Size(1, 1), CV_8UC3, cv::Scalar(r, g, b));
-                cv::cvtColor(color_rgb, color_hsv, cv::COLOR_RGB2HSV);
-                const cv::Vec<uint8_t, 3> hsv = color_hsv.at<cv::Vec<uint8_t, 3>>(0, 0);
-                const auto cur_h = hsv[0];
-                const auto cur_s = hsv[1];
-                const auto cur_v = hsv[2];
+                const cv::Vec<uint8_t, 3> color_hsv = rgb2hsv_lut.at<cv::Vec<uint8_t, 3>>(r + lut_dim*g + lut_dim*lut_dim*b);
+                const auto cur_h = color_hsv[0];
+                const auto cur_s = color_hsv[1];
+                const auto cur_v = color_hsv[2];
                 const bool h_ok = (!overflow && cur_h > hue_lower && cur_h < hue_higher) || (overflow && (cur_h > hue_lower || cur_h < hue_higher));
                 const bool s_ok = cur_s > sat_lower && cur_s < sat_higher;
                 const bool v_ok = cur_v > val_lower && cur_v < val_higher;
@@ -49,15 +47,16 @@ namespace object_detect
         }
 
     private:
-          const double hue_lower;
-          const double hue_higher;
-          const bool overflow;
-          const double sat_lower;
-          const double sat_higher;
-          const double val_lower;
-          const double val_higher;
-          const lut_elem_t color_label;
-          lut_t& lut;
+        const cv::Mat rgb2hsv_lut;
+        const double hue_lower;
+        const double hue_higher;
+        const bool overflow;
+        const double sat_lower;
+        const double sat_higher;
+        const double val_lower;
+        const double val_higher;
+        const lut_elem_t color_label;
+        lut_t& lut;
   };
 
   //}
@@ -67,6 +66,22 @@ namespace object_detect
     double hue_lower = seg_conf.hue_center - seg_conf.hue_range / 2.0;
     double hue_higher = seg_conf.hue_center + seg_conf.hue_range / 2.0;
     bool overflow;
+    static cv::Mat rgb2hsv_lut;
+    if (rgb2hsv_lut.empty())
+    {
+      rgb2hsv_lut = cv::Mat(1, lut_size, CV_8UC3);
+      for (size_t r = 0; r < lut_dim; r++)
+      {
+        for (size_t g = 0; g < lut_dim; g++)
+        {
+          for (size_t b = 0; b < lut_dim; b++)
+          {
+            rgb2hsv_lut.at<cv::Vec<uint8_t, 3>>(r + lut_dim*g + lut_dim*lut_dim*b) = cv::Vec<uint8_t, 3>(r, g, b);
+          }
+        }
+      }
+      cv::cvtColor(rgb2hsv_lut, rgb2hsv_lut, cv::COLOR_RGB2HSV);
+    }
     /* calculate the correct bounds for the pixel HSV values //{ */
     {
       overflow = false;
@@ -90,6 +105,7 @@ namespace object_detect
     const double val_higher = seg_conf.val_center + seg_conf.val_range / 2.0;
 
     parallel_for_(cv::Range(0, lut_dim), parallelHSVLUT(
+          rgb2hsv_lut,
           hue_lower, hue_higher, overflow,
           sat_lower, sat_higher,
           val_lower, val_higher,
@@ -106,11 +122,13 @@ namespace object_detect
   {
     public:
         parallelLabLUT(
+            cv::Mat rgb2lab_lut,
             const double l_lower, const double l_higher,
             const double a_lower, const double a_higher,
             const double b_lower, const double b_higher,
             lut_elem_t color_label, lut_t& lut)
-          : l_lower(l_lower), l_higher(l_higher),
+          : rgb2lab_lut(rgb2lab_lut),
+            l_lower(l_lower), l_higher(l_higher),
             a_lower(a_lower), a_higher(a_higher),
             b_lower(b_lower), b_higher(b_higher),
             color_label(color_label), lut(lut)
@@ -119,20 +137,17 @@ namespace object_detect
 
         void operator()(const cv::Range &range) const
         {
-          cv::Mat color_rgb;
-          cv::Mat color_lab;
+          cv::Vec<uint8_t, 3> color_rgb;
           for (auto r = range.start; r < range.end; r++)
           {
             for (size_t g = 0; g < lut_dim; g++)
             {
               for (size_t b = 0; b < lut_dim; b++)
               {
-                color_rgb = cv::Mat(cv::Size(1, 1), CV_8UC3, cv::Scalar(r, g, b));
-                cv::cvtColor(color_rgb, color_lab, cv::COLOR_RGB2Lab);
-                const cv::Vec<uint8_t, 3> lab = color_lab.at<cv::Vec<uint8_t, 3>>(0, 0);
-                const auto cur_l = lab[0];
-                const auto cur_a = lab[1];
-                const auto cur_b = lab[2];
+                const cv::Vec<uint8_t, 3> color_lab = rgb2lab_lut.at<cv::Vec<uint8_t, 3>>(r + lut_dim*g + lut_dim*lut_dim*b);
+                const auto cur_l = color_lab[0];
+                const auto cur_a = color_lab[1];
+                const auto cur_b = color_lab[2];
                 const bool l_ok = cur_l > l_lower && cur_l < l_higher;
                 const bool a_ok = cur_a > a_lower && cur_a < a_higher;
                 const bool b_ok = cur_b > b_lower && cur_b < b_higher;
@@ -144,20 +159,38 @@ namespace object_detect
         }
 
     private:
-          const double l_lower;
-          const double l_higher;
-          const double a_lower;
-          const double a_higher;
-          const double b_lower;
-          const double b_higher;
-          const lut_elem_t color_label;
-          lut_t& lut;
+        const cv::Mat rgb2lab_lut;
+        const double l_lower;
+        const double l_higher;
+        const double a_lower;
+        const double a_higher;
+        const double b_lower;
+        const double b_higher;
+        const lut_elem_t color_label;
+        lut_t& lut;
   };
 
   //}
 
   void add_lut_lab(lut_t& ret, const SegConf& seg_conf)
   {
+    static cv::Mat rgb2lab_lut;
+    if (rgb2lab_lut.empty())
+    {
+      rgb2lab_lut = cv::Mat(1, lut_size, CV_8UC3);
+      for (size_t r = 0; r < lut_dim; r++)
+      {
+        for (size_t g = 0; g < lut_dim; g++)
+        {
+          for (size_t b = 0; b < lut_dim; b++)
+          {
+            rgb2lab_lut.at<cv::Vec<uint8_t, 3>>(r + lut_dim*g + lut_dim*lut_dim*b) = cv::Vec<uint8_t, 3>(r, g, b);
+          }
+        }
+      }
+      cv::cvtColor(rgb2lab_lut, rgb2lab_lut, cv::COLOR_RGB2Lab);
+    }
+
     const double l_lower = seg_conf.l_center - seg_conf.l_range / 2.0;
     const double l_higher = seg_conf.l_center + seg_conf.l_range / 2.0;
 
@@ -168,6 +201,7 @@ namespace object_detect
     const double b_higher = seg_conf.b_center + seg_conf.b_range / 2.0;
 
     parallel_for_(cv::Range(0, lut_dim), parallelLabLUT(
+          rgb2lab_lut,
           l_lower, l_higher,
           a_lower, a_higher,
           b_lower, b_higher,
