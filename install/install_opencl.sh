f0b8033345a03b831b2f33a606427586cc963a4d
Matouš Vrba <vrbamato@fel.cvut.cz>
2021-03-02 19:27:15 +0100

    updated the install script

---
+++ install/install_opencl.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt install ocl-icd-opencl-dev ocl-icd-libopencl1 intel-opencl-icd
--- scripts/install_opencl.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt update
-sudo apt install ocl-icd-opencl-dev
-sudo apt install ocl-icd-libopencl1
